0266ef29613803f46828012a2ef1668ce36d1e9f
Edilton Junior <ton@espiralinterativa.com>
2019-07-25 11:49:12 -0300

    atualizando

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
+import { LoginService } from './login.service';
+import {NotificationService} from '../../shared/messages/notification.service'
+
+import {User} from './user.model'
+
 @Component({
   selector: 'mt-login',
   templateUrl: './login.component.html',
@@ -10,13 +15,24 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private loginService: LoginService,
+              private notificationService: NotificationService) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       email: this.fb.control('', [Validators.required, Validators.email]),
       password: this.fb.control('', [Validators.required])
     })
+  }
+
+  login(){
+    this.loginService.login(this.loginForm.value.email,
+                            this.loginForm.value.password)
+                      .subscribe(user => 
+                                    this.notificationService.notify(`Bem vindo, ${user.name}`),
+                                 response => //HttpErrorResponse
+                                    this.notificationService.notify(response.error.message))
   }
 
 }
